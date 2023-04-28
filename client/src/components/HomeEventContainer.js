c2f8131a7d32d4cc89fa433c869375755b2efaef
Henry Yun <henrysungyun@gmail.com>
2023-04-28 16:46:57 -0700

    wow just wow

--- client/src/components/HomeEventContainer.js
+++ client/src/components/HomeEventContainer.js
@@ -1,27 +1,53 @@
 import React, { useEffect } from 'react';
 import { useRecoilState } from 'recoil';
-import { roomState } from '../atoms/index';
+import { isLoadingState, currentlyLoggedInState, roomState } from '../atoms/index';
 import HomeEventCard from './HomeEventCard';
 
 function HomeEventContainer() {
+    const [loggedInUser, setLoggedInUser] = useRecoilState(currentlyLoggedInState);
     const [rooms, setRooms] = useRecoilState(roomState);
+    const [isLoading, setIsLoading] = useRecoilState(isLoadingState);
 
   useEffect(() => {
-    // Fetch rooms from API and update recoil state
     const fetchRooms = async () => {
       const response = await fetch('/rooms');
-      const data = await response.json();
-      setRooms(data.rooms);
+      const roomData = await response.json();
+      setRooms(roomData);
+      setIsLoading(false);
     };
+    
     fetchRooms();
   }, []);
 
+  if (!loggedInUser) {
+    return <div>Loading...</div>;
+  }
+
+  const filteredRooms = rooms.filter((room) => {
+    if (loggedInUser.id === room.created_by) {
+      return true;
+    }
+    return false;
+  });
+
+  console.log(rooms)
+
   return (
-    <div className="card-container">
-      {rooms.map((room) => (
-        <HomeEventCard key={room.id} room={room} />
-      ))}
-    </div>
+    <>
+      {filteredRooms.length === 0 ? (
+        <div>
+          <h1>No events planned</h1>
+        </div>
+      ) : (
+        <div className="card-container">
+          {rooms
+            .filter((room) => loggedInUser.id === room.created_by)
+            .map((room) => (
+              <HomeEventCard key={room.id} room={room} />
+            ))}
+        </div>
+      )}
+    </>
   );
 };
 
