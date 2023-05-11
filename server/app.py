6984c2495bc38dc9d7a942c44318ea32d85b003d
Henry Yun <henrysungyun@gmail.com>
2023-05-10 17:09:05 -0700

    moved config file

--- server/app.py
+++ server/app.py
@@ -1,12 +1,33 @@
 #!/usr/bin/env python3
 from flask import Flask, jsonify, request, make_response, session
+from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from flask_bcrypt import Bcrypt
 from sqlalchemy.exc import IntegrityError
 
-from config import *
+# from config import *
 from models import User, Friend, Participant, Event_Planning_Room, Event_Element, Message
 from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+app.secret_key = b'~`25<q\x84-\xa9t\xb5\xc4WX\xafB'
+
+db = SQLAlchemy (metadata=metadata)
+migrate = Migrate (app, db)
+db.init_app(app)
+
+api = Api(app)
+
+CORS(app)
+
+bcrypt = Bcrypt(app)
 
 class Home(Resource):
     def get(self):
--- server/config.py
+++ server/config.py
@@ -1,37 +1,37 @@
-# standard library imports
-import os
+# # standard library imports
+# import os
 
-# remote library imports
-from flask import Flask
-from flask_cors import CORS
-from flask_migrate import Migrate
-from flask_restful import Api
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
-from flask_bcrypt import Bcrypt
+# # remote library imports
+# from flask import Flask
+# from flask_cors import CORS
+# from flask_migrate import Migrate
+# from flask_restful import Api
+# from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy import MetaData
+# from flask_bcrypt import Bcrypt
 
-# instantiate app, set attributes
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
+# # instantiate app, set attributes
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.json.compact = False
 
-app.secret_key = b'~`25<q\x84-\xa9t\xb5\xc4WX\xafB'
+# app.secret_key = b'~`25<q\x84-\xa9t\xb5\xc4WX\xafB'
 
-# define metadata, instantiate db
-metadata = MetaData(naming_convention={
-    'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
-})
+# # define metadata, instantiate db
+# metadata = MetaData(naming_convention={
+#     'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
+# })
 
-db = SQLAlchemy (metadata=metadata)
-migrate = Migrate (app, db)
-db.init_app(app)
+# db = SQLAlchemy (metadata=metadata)
+# migrate = Migrate (app, db)
+# db.init_app(app)
 
-# instantiate REST api
-api = Api(app)
+# # instantiate REST api
+# api = Api(app)
 
-# instantiate CORS
-CORS(app)
+# # instantiate CORS
+# CORS(app)
 
-#instantiate BCrypt
-bcrypt = Bcrypt(app)
+# #instantiate BCrypt
+# bcrypt = Bcrypt(app)
